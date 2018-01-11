c542417dd6f20830a98aa0226938ccf505b0da17
danchaotaiyang <yuxiaolong00@qq.com>
2018-01-11 10:20:13 +0800

    Update

---
+++ src/assets/js/jsonp.js
@@ -0,0 +1,25 @@
+const origin = require('src/assets/js/jsonp');
+
+const param = (data) => {
+    let url = '';
+    for (let k in data) {
+        let v = data[k] !== 'undefined' ? data[k] : '';
+        url += `&${k}=${encodeURIComponent(v)}`;
+    }
+    return url ? url.substring(1) : '';
+};
+
+const jsonp = (url, data, option) => {
+    url += (url.indexOf('?') < 0 ? '?' : '&') + param(data);
+    return new Promise((resolve, reject) => {
+        origin(url, option, (err, data) => {
+            if (!err) {
+                resolve(data);
+            } else {
+                reject(data);
+            }
+        });
+    });
+};
+
+export default jsonp;
